bc5e9fa011a626a9f94247ea26ff9e6fc6c8ee52
Faisal Alsheet <105297937+xVolkov@users.noreply.github.com>
2023-11-22 04:31:49 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_calories_calculator
+
+This is a simple meal planner application that calculates calories and saves meal plans in a date-organized manner.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "Z:\\Programs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=Z:\\Programs\\flutter"
+  "PROJECT_DIR=D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator"
+  "FLUTTER_ROOT=Z:\\Programs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator"
+  "FLUTTER_TARGET=D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz1mYWxzZQ==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNTQ1Zjg3MDVkZjMwMTg3N2Q3ODcxMDdiYWMxYTZlOWZjOWVlMWFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\My File\\UNI\\_Software Engineering\\5th Year\\Fall\\Mobile App\\Assignments\\A3_100639174\\flutter_calories_calculator\\.dart_tool\\package_config.json"
+)
